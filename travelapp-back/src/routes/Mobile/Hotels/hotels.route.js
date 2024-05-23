65f22c7c8cb1b0bda3625915f43202ca656a4458
Abdelaziz <abdeez.aushar@yandex.com>
2024-05-23 23:35:58 +0300

    Fixes

--- travelapp-back/src/routes/Mobile/Hotels/hotels.route.js
+++ travelapp-back/src/routes/Mobile/Hotels/hotels.route.js
@@ -1,15 +1,15 @@
 
 const asyncHandler = require('express-async-handler');
-const requireJwtAuth = require('../../middlewares/checkJwtAuth');
-const checkObjectID = require('../../middlewares/checkObjectID');
+const requireJwtAuth = require('../../../middlewares/checkJwtAuth');
+const checkObjectID = require('../../../middlewares/checkObjectID');
 const express = require('express');
 const router = express.Router();
 
 
 const { makeReservation, searchHotels, getHotels } = require('./hotels.controller');
 
-router.post('/reserve', makeReservation);
-router.get('/search', searchHotels);
+router.post('/reserve', requireJwtAuth, makeReservation);
+router.get('/search', requireJwtAuth, searchHotels);
 
 
 module.exports = router;
