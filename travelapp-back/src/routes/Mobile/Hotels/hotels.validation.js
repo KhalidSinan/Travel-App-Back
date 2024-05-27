f4629722a5e9ed6b6eedbbcf49473f3bf9c060ef
TimaDawaa <tima302t@gmail.com>
2024-05-27 11:40:00 +0300

    fix

--- travelapp-back/src/routes/Mobile/Hotels/hotels.validation.js
+++ travelapp-back/src/routes/Mobile/Hotels/hotels.validation.js
@@ -1,14 +1,14 @@
 const Joi = require("joi");
 
 const searchHotelsValidation = Joi.object({
-    nameOrCity: Joi.string().required(),
-    startDate: Joi.string().optional(),
-    numDays: Joi.number().integer().min(1).optional(),
-    numRooms: Joi.number().integer().min(1).optional(),
-    stars: Joi.number().integer().optional(),
-    sortField: Joi.string().valid('price', 'stars', 'nothing').optional(),
-    order: Joi.string().valid('asc', 'desc').optional(),
-    page: Joi.number().integer().min(1).optional()
+  nameOrCity: Joi.string().required(),
+  startDate: Joi.string().optional(),
+  numDays: Joi.number().integer().min(1).optional(),
+  numRooms: Joi.number().integer().min(1).optional(),
+  stars: Joi.number().integer().optional(),
+  sortField: Joi.string().valid('price', 'stars', 'nothing').optional(),
+  order: Joi.string().valid('asc', 'desc').optional(),
+  page: Joi.number().integer().min(1).optional()
 });
 
 const reservationValidation = Joi.object({
