8e351b107c78c488b683649be29a9bd67496d431
Abdelaziz <abdeez.aushar@yandex.com>
2024-06-01 19:39:28 +0300

    Added Places Routes

---
+++ travelapp-back/src/routes/Mobile/Places/places.controller.js
@@ -0,0 +1,54 @@
+
+const Place = require('../../../models/places.mongo');
+const User = require('../../../models/users.mongo');
+
+async function getAllByCity(req, res) {
+    const { city, category } = req.body;
+    let query = {};
+
+    if (!category) {
+        return res.status(400).json({ message: 'Category parameter is required' });
+    }
+    query.category = category;
+
+    if (city) {
+        query['address.city'] = city;
+    }
+
+    try {
+        const places = await Place.find(query);
+        res.status(200).json(places);
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving places', error });
+    }
+}
+
+async function getAllNearby(req, res) {
+    const { userId, category } = req.body;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (!user.location || !user.location.city) {
+            return res.status(400).json({ message: 'User location city is not set' });
+        }
+
+        let query = { 'address.city': user.location.city };
+
+        if (category) {
+            query.category = category;
+        }
+
+        const places = await Place.find(query);
+        res.status(200).json(places);
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving nearby places', error });
+    }
+}
+
+module.exports = {
+    getAllByCity,
+    getAllNearby,
+};
