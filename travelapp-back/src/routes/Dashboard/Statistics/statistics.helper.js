359c869f5c0d3db088f245d57351100588339c77
Abdelaziz <abdeez.aushar@yandex.com>
2024-07-24 21:30:50 +0300

    Fixes
    Top 10 Countries Sort

--- travelapp-back/src/routes/Dashboard/Statistics/statistics.helper.js
+++ travelapp-back/src/routes/Dashboard/Statistics/statistics.helper.js
@@ -24,6 +24,7 @@ async function getTop10CountriesHelper(countries) {
                 lastMonthCount: tempCount
             })
         }))
+    data.sort((a, b) => b.count - a.count);
     return data;
 }
 
