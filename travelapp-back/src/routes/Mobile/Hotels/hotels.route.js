f44a25e96c34fc022fb733366cf10e55058beb14
Abdelaziz <abdeez.aushar@yandex.com>
2024-07-28 19:34:54 +0300

    Features
    Hotel Reservation Done

--- travelapp-back/src/routes/Mobile/Hotels/hotels.route.js
+++ travelapp-back/src/routes/Mobile/Hotels/hotels.route.js
@@ -6,11 +6,12 @@ const express = require('express');
 const router = express.Router();
 
 
-const { makeReservation, searchHotels, payReservation, getCountriesWithCities, getHotelsByCities, httpGetHotelReservations } = require('./hotels.controller');
+const { makeReservation, searchHotels, payReservation, getCountriesWithCities, getHotelsByCities, httpGetHotelReservations, httpGetHotelReservation } = require('./hotels.controller');
 
 router.get('/cities', asyncHandler(getCountriesWithCities));
 router.post('/reserve', requireJwtAuth, asyncHandler(makeReservation));
 router.get('/my-reservations', requireJwtAuth, asyncHandler(httpGetHotelReservations));
+router.get('/my-reservations/:id', requireJwtAuth, checkObjectID, asyncHandler(httpGetHotelReservation));
 router.post('/search', requireJwtAuth, asyncHandler(searchHotels));
 router.post('/search/cities', requireJwtAuth, asyncHandler(getHotelsByCities));
 router.get('/reservation/:id/pay', checkObjectID, asyncHandler(payReservation));
