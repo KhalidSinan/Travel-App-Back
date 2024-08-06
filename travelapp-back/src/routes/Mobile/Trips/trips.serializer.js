87c1b019f4c175df62c3e3bce51fe51a821c370b
Abdelaziz <abdeez.aushar@yandex.com>
2024-08-06 11:43:50 +0300

    Fixes
    Trip Data
    Organized Trip Data

--- travelapp-back/src/routes/Mobile/Trips/trips.serializer.js
+++ travelapp-back/src/routes/Mobile/Trips/trips.serializer.js
@@ -5,7 +5,8 @@ function tripData(trip) {
         start_date: trip.start_date.toLocaleDateString('en-GB'),
         overall_price: trip.overall_price,
         num_of_people: trip.num_of_people,
-        num_of_days: trip.overall_num_of_days
+        num_of_days: trip.overall_num_of_days,
+        completed: trip.end_date < new Date() ? true : false
     }
 }
 
