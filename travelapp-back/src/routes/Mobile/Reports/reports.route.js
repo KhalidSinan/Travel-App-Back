4ff92968c297d526ba8f01b38376f26c7f44ecdb
Abdelaziz <abdeez.aushar@yandex.com>
2024-08-12 16:57:21 +0300

    Fixes
    Report On App
    Report On Organizer

--- travelapp-back/src/routes/Mobile/Reports/reports.route.js
+++ travelapp-back/src/routes/Mobile/Reports/reports.route.js
@@ -2,11 +2,13 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 
 const requireJwtAuth = require('../../../middlewares/checkJwtAuth');
-const { httpPostReport, httpGetOrganizers } = require('./reports.controller');
+const { httpGetOrganizers, httpPostReportOnApp, httpPostReportOnOrganizer } = require('./reports.controller');
+const checkObjectID = require('../../../middlewares/checkObjectID');
 
 const reportRouter = express.Router();
 
-reportRouter.post('/', requireJwtAuth, asyncHandler(httpPostReport));
+reportRouter.post('/app', requireJwtAuth, asyncHandler(httpPostReportOnApp));
+reportRouter.post('/organizers/:id', requireJwtAuth, checkObjectID, asyncHandler(httpPostReportOnOrganizer));
 reportRouter.get('/organizers', requireJwtAuth, asyncHandler(httpGetOrganizers));
 
 module.exports = reportRouter;
